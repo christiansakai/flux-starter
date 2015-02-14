4e8941dac8e5ec0589d6ad93aae00d35bdbe22c2
Sam McCord <samuel.e.mccord@gmail.com>
2015-02-14 03:12:12 -0500

    intiial

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('browserify', function() {
+    gulp.src('src/js/main.js')
+        .pipe(browserify({transform: 'reactify'}))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy', function() {
+    gulp.src('src/index.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('buildCSS', function () {
+    gulp.src('src/sass/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default',['browserify', 'copy', 'buildCSS']);
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.*', ['default']);
+});
